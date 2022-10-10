99bac4270087e249ef326941ac96ac0e417f4f33
Nicholas Schweitzer <f3bl@gmx.com>
2022-10-10 11:52:58 +0200

    Different surface counting method that doesn't segfault

--- vkwc.c
+++ vkwc.c
@@ -95,7 +95,22 @@ struct Server {
 	struct wl_listener new_xwayland_surface;
 
 	struct wl_list windows;
+
+	struct wl_listener new_surface;
+
+	struct wl_list surfaces;
 };
+
+struct surface {
+	struct wlr_surface *wlr;
+	struct wlr_scene_surface *scene_surface;
+	struct wlr_scene_rect *border;
+	struct wl_list link;
+
+	struct wl_listener commit;
+	struct wl_listener destroy;
+};
+
 
 struct Output {
 	struct wl_list link;
@@ -137,9 +152,65 @@ struct Keyboard	{
 	struct wl_listener key;
 };
 
-struct Surface {
-	struct wl_listener destroy;
-};
+/*
+static void handle_destroy_surface(struct wl_listener *listener, void *data) {
+	printf("Surface destroyed\n");
+	fflush(stdout);
+}
+
+static void handle_new_surface(struct wl_listener *listener, void *data) {
+	printf("New surface!\n");
+	fflush(stdout);
+
+	struct wlr_surface *surface = data;
+	struct Server *server;
+	server = wl_container_of(listener, server, new_surface);
+	server->destroy_surface.notify = handle_destroy_surface;
+	wl_signal_add(&surface->events.commit, &server->destroy_surface);
+}
+*/
+
+static void surface_handle_destroy(struct wl_listener *listener, void *data) {
+	printf("destroy surface\n");
+	fflush(stdout);
+	struct surface *surface = wl_container_of(listener, surface, destroy);
+	//wlr_scene_node_destroy(&surface->scene_surface->node);
+	//wlr_scene_node_destroy(&surface->border->node);
+	//wl_list_remove(&surface->destroy.link);
+	//wl_list_remove(&surface->link);
+	free(surface);
+}
+
+static void server_handle_new_surface(struct wl_listener *listener,
+		void *data) {
+	printf("new surface\n");
+	fflush(stdout);
+	struct Server *server = wl_container_of(listener, server, new_surface);
+	struct wlr_surface *wlr_surface = data;
+
+	int pos = 50 * wl_list_length(&server->surfaces);
+
+	struct surface *surface = calloc(1, sizeof(struct surface));
+	surface->wlr = wlr_surface;
+	//surface->commit.notify = surface_handle_commit;
+	//wl_signal_add(&wlr_surface->events.commit, &surface->commit);
+	surface->destroy.notify = surface_handle_destroy;
+	wl_signal_add(&wlr_surface->events.destroy, &surface->destroy);
+
+	/* Border dimensions will be set in surface.commit handler */
+	/*
+	surface->border = wlr_scene_rect_create(&server->scene->node,
+			0, 0, (float[4]){ 0.5f, 0.5f, 0.5f, 1 });
+	wlr_scene_node_set_position(&surface->border->node, pos, pos);
+
+	surface->scene_surface =
+		wlr_scene_surface_create(&server->scene->node, wlr_surface);
+	wl_list_insert(server->surfaces.prev, &surface->link);
+
+	wlr_scene_node_set_position(&surface->scene_surface->node,
+			pos + 3, pos + 3);
+	*/
+}
 
 static void focus_view(struct View *view, struct wlr_surface *surface) {
 	/* Note: this function only deals with keyboard	focus. */
@@ -872,6 +943,14 @@ int main(int argc, char	*argv[]) {
 	 * see the handling of the request_set_selection event below.*/
 	struct wlr_compositor *compositor = wlr_compositor_create(server.wl_display, server.renderer);
 	wlr_data_device_manager_create(server.wl_display);
+
+	// Surface counting stuff
+	//server.new_surface.notify = handle_new_surface;
+	//wl_signal_add(&compositor->events.new_surface, &server.new_surface);
+
+	wl_list_init(&server.surfaces);
+	server.new_surface.notify = server_handle_new_surface;
+	wl_signal_add(&compositor->events.new_surface, &server.new_surface);
 
 	/* Creates an output layout, which a wlroots utility for working with an
 	 * arrangement of screens in a physical	layout.	*/
