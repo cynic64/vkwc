70f78ac489b456ea073fc05fb3abdcc4cac9c778
Nicholas Schweitzer <f3bl@gmx.com>
2022-10-12 09:34:11 +0200

    More deletion of obsolete functions

--- util.h
+++ util.h
@@ -8,7 +8,4 @@ struct wlr_scene_node *get_toplevel_node(struct wlr_scene_node *node);
 
 void print_scene_graph(struct wlr_scene_node *node, int	level);
 
-void print_matrix(mat4 matrix);
-
 #endif // util_h_INCLUDED
-
